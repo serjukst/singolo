6aa8897ca45224da48258353526dbb64e1e3cde2
Serhii Stepanchuk <serjukst@gmail.com>
2020-03-13 17:54:56 +0200

    feat: finish animations in  slider section

--- index.js
+++ index.js
@@ -7,42 +7,86 @@ NAV.addEventListener('click', (event) => {
 });
 
 // Slider
-const PREV_LINK = document.getElementById("prev_link");
-const NEXT_LINK = document.getElementById("next_link");
-let sliderItem2 = document.getElementsByClassName("slider__item-2")[0];
-console.log(sliderItem2.style.display == 'flex');
-let sliderSection = document.getElementById('slider');
 
-let slideIndex = 1;
+let items = document.querySelectorAll('.slider__item');
+let slider = document.getElementById('slider');
+let currentItem = 0;
+let isEnabled = true;
 
-PREV_LINK.addEventListener('click', () => {
-    showSlides(slideIndex -= 1);  
-});
-PREV_LINK.addEventListener('click', () => {
-    sliderSection.classList.add('slider_colored');
-});
-
-NEXT_LINK.addEventListener('click', () => {
-    showSlides(slideIndex += 1);
-});
-
-NEXT_LINK.addEventListener('click', () => {
-    sliderSection.classList.remove('slider_colored');
-});
-
-function showSlides(n) {
-    
-    let i,
-        slides = document.getElementsByClassName("slider__item");
-
-    if (n > slides.length) { slideIndex = 1};
-
-    if (n < 1) { slideIndex = slides.length; };
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    
-    slides[slideIndex - 1].style.display = "flex";
+function changeCurrentItem(n) {
+	currentItem = (n + items.length) % items.length;
 }
 
+function hideItem(direction) {
+	isEnabled = false;
+	items[currentItem].classList.add(direction);
+	items[currentItem].addEventListener('animationend', function() {
+		this.classList.remove('active', direction);
+	});
+}
+
+function showItem(direction) {
+	items[currentItem].classList.add('next', direction);
+	items[currentItem].addEventListener('animationend', function() {
+		this.classList.remove('next', direction);
+		this.classList.add('active');
+		isEnabled = true;
+    });
+
+    if(items[currentItem].classList.contains('slider__item-2')) {
+        slider.classList.add('slider_colored');
+    } else {
+        slider.classList.remove('slider_colored');
+    };
+
+};
+
+function nextItem(n) {
+	hideItem('to-left');
+	changeCurrentItem(n + 1);
+	showItem('from-right');
+};
+
+function previousItem(n) {
+	hideItem('to-right');
+	changeCurrentItem(n - 1);
+	showItem('from-left');
+};
+
+document.querySelector('.prev_link').addEventListener('click', function() {
+	if (isEnabled) {
+		previousItem(currentItem);
+	}
+});
+
+document.querySelector('.next_link').addEventListener('click', function() {
+	if (isEnabled) {
+		nextItem(currentItem);
+	}
+});
+
+//Phone 
+
+let horizontalPhone = document.querySelectorAll('.slider__horizontal-image')[0];
+let horizontalPhoneDark = horizontalPhone.children[1];
+
+horizontalPhone.addEventListener('click', function () {
+
+    if(horizontalPhoneDark.classList.contains('visible')) {
+        horizontalPhoneDark.classList.remove('visible');
+    } else {
+        horizontalPhoneDark.classList.add('visible');
+    };
+});
+
+let verticalPhone = document.querySelectorAll('.slider__vertical-image')[0];
+let verticalPhoneDark = verticalPhone.children[1];
+
+
+verticalPhone.addEventListener('click', function () {
+    if(verticalPhoneDark.classList.contains('visible')) {
+        verticalPhoneDark.classList.remove('visible');
+    } else {
+        verticalPhoneDark.classList.add('visible');
+    };
+});
